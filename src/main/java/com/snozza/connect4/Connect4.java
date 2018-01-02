440eab3e9037df99ea6d30a97a7a87b2d808c69b
Andrew <andrewcsnead@gmail.com>
2018-01-02 12:45:17 +0000

    add print board stream

--- src/main/java/com/snozza/connect4/Connect4.java
+++ src/main/java/com/snozza/connect4/Connect4.java
@@ -1,30 +1,63 @@
 package com.snozza.connect4;
 
+import java.io.PrintStream;
 import java.util.Arrays;
+import java.util.StringJoiner;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Connect4 {
 
     private static final int ROWS = 6;
     private static final int COLUMNS = 7;
     private static final String EMPTY = " ";
+    private static final String RED = "R";
+    private static final String GREEN = "G";
+    private static final String DELIMITER = "|";
+
+    private PrintStream outputChannel;
+    private String currentPlayer = "R";
     private String[][] board = new String[ROWS][COLUMNS];
 
-    public Connect4() {
+    public Connect4(PrintStream out) {
+        outputChannel = out;
         for (String[] row : board) {
             Arrays.fill(row, EMPTY);
         }
+    }
+
+    public String getCurrentPlayer() {
+        outputChannel.printf("Player %s turn%n", currentPlayer);
+        return currentPlayer;
+    }
+
+    private void printBoard() {
+       for (int row = ROWS - 1; row >= 0; row--) {
+           StringJoiner stringJoiner = new StringJoiner(DELIMITER, DELIMITER, DELIMITER);
+           Stream.of(board[row]).forEachOrdered(stringJoiner::add);
+           outputChannel.println(stringJoiner.toString());
+       }
     }
 
     public int getNumberOfDiscs() {
         return IntStream.range(0, COLUMNS).map(this::getNumberOfDiscsInColumn).sum();
     }
 
+    private void switchPlayer() {
+        if (RED.equals(currentPlayer)) {
+            currentPlayer = GREEN;
+        } else {
+            currentPlayer = RED;
+        }
+    }
+
     public int putDiscInColumn(int column) {
         checkColumn(column);
         int row = getNumberOfDiscsInColumn(column);
         checkInsertPosition(row, column);
-        board[row][column] = "X";
+        board[row][column] = getCurrentPlayer();
+        printBoard();
+        switchPlayer();
         return row;
     }
 
--- src/test/java/com/snozza/connect4/Connect4Spec.java
+++ src/test/java/com/snozza/connect4/Connect4Spec.java
@@ -5,20 +5,26 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import static org.hamcrest.Matchers.is;
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class Connect4Spec {
 
     private Connect4 tested;
+    private OutputStream output;
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
     @Before
     public void beforeEachTest() {
-        tested = new Connect4();
+        output = new ByteArrayOutputStream();
+        tested = new Connect4(new PrintStream(output));
     }
 
     @Test
@@ -64,5 +70,30 @@ public class Connect4Spec {
         exception.expect(RuntimeException.class);
         exception.expectMessage("No more room in column " + column);
         tested.putDiscInColumn(column);
+    }
+
+    @Test
+    public void whenFirstPlayerPlaysThenDiscColorIsRed() {
+        assertThat(tested.getCurrentPlayer(), is("R"));
+    }
+
+    @Test
+    public void whenSecondPlayerPlaysThenDiscColorIsGreen() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(tested.getCurrentPlayer(), is("G"));
+    }
+
+    @Test
+    public void whenAskedForCurrentPlayerThePlayerIsOutput() {
+        tested.getCurrentPlayer();
+        assertThat(output.toString(), containsString("Player R turn"));
+    }
+
+    @Test
+    public void whenADiscIsIntroducedTheBoardIsPrinted() {
+        int column = 1;
+        tested.putDiscInColumn(column);
+        assertThat(output.toString(), containsString("| |R| | | | | |"));
     }
 }
